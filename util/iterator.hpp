66bcb34bb76b16889c17eff6e019696ff4afbeab
Ilyass Lefhaili <ilefhail@e1r5p1.1337.ma>
2023-01-30 14:00:24 +0100

    working in iterator

--- util/iterator.hpp
+++ util/iterator.hpp
@@ -2,10 +2,32 @@
 #define V_hpp
 
 namespace ft{
+    template <class Iterator>
+    struct iterator_traits{
+        typedef typename Iterator::value_type                   value_type;
+        typedef typename Iterator::difference_type              difference_type;
+        typedef typename Iterator::iterator_category            iterator_category;
+        typedef typename Iterator::pointer                      pointer;
+        typedef typename Iterator::reference                    reference;
+    };
+    template <class T*>
+    struct iterator_traits{
+        typedef  T                          value_type;
+        typedef size_t                      difference_type;
+        typedef random_access_iterator_tag  iterator_category;
+        typedef T*                          pointer;
+        typedef T&                          reference;
+    };
+    struct iterator_traits
     template <class T>
     class iterator{
+        typedef typename iterator_traits<T *>::value_type               value_type;
+        typedef typename iterator_traits<T *>::difference_type          difference_type;
+        typedef typename iterator_traits<T *>::iterator_category        iterator_category;
+        typedef typename iterator_traits<T *>::pointer                  pointer;
+        typedef typename iterator_traits<T *>::reference                reference; 
         private:
-            T* ptr;
+            pointer ptr;
         public:
             iterator() {
                 ptr = NULL;
@@ -16,12 +38,85 @@ namespace ft{
             iterator(iterator& p){
                 *this = p;
             }
-            iterator& operator=(const iterator& to_copy){
+            T& operator=(const iterator& to_copy){
                 ptr = to_copy.ptr;
                 return (*this);
             }
-            bool operator==(iterator )
-    }
+            bool operator==(iterator &a)
+            {
+                return ptr == a.ptr;
+            }
+            bool operator!=(iterator a)
+            {
+                return ptr != a.ptr;
+            }
+            bool operator<(iterator &a)
+            {
+                return ptr < a.ptr;
+            }
+            bool operator>(iterator &a)
+            {
+                return ptr > a.ptr;
+            }
+            bool operator<=(iterator &a)
+            {
+                return ptr <= a.ptr;
+            }
+            bool operator>=(iterator &a)
+            {
+                return ptr >= a.ptr;
+            }
+            T* operator->()
+            {
+                return (ptr);
+            }
+            T& operator*()
+            {
+                return (*ptr);
+            }
+            iterator& operator+(int i)
+            {
+                ptr += i;
+                return (*this);
+            }
+            iterator& operator-(int i)
+            {
+                ptr -= i;
+                return (*this);
+            }
+            iterator& operator+(iterator& i)
+            {
+                ptr += i.ptr;
+                return (*this);
+            }
+            iterator& operator-(iterator& i)
+            {
+                ptr -= i.ptr;
+                return (*this);
+            }
+            iterator& operator++()
+            {
+                ptr++;
+                return (*this);
+            }
+            iterator operator++(int)
+            {
+                iterator temp = *this;
+                ++ptr;
+                return (temp);
+            }
+            iterator& operator--()
+            {
+                ptr--;
+                return (*this);
+            }
+            iterator operator--(int)
+            {
+                iterator temp = *this;
+                --ptr;
+                return (temp);
+            }
+    };
 }
 
 
---
+++ util/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include "iterator.hpp"
+int main()
+{
+    ft::iterator<int> a;
+    std::iterator_traits<int> l;
+    ft::iterator<int> b;
+
+    if (a >= b)
+        std::cout<<" lsfjklsaj\n";
+}
