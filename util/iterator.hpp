4bf752bc1b8165641aa29d25a0c326947f4c388e
Ilyass Lefhaili <ilefhail@e1r4p5.1337.ma>
2023-01-30 15:22:50 +0100

    iterator_traits is almost done

--- util/iterator.hpp
+++ util/iterator.hpp
@@ -1,24 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iterator.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilefhail <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/30 15:13:04 by ilefhail          #+#    #+#             */
+/*   Updated: 2023/01/30 15:13:07 by ilefhail         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef V_hpp
 #define V_hpp
 
+#include "iterator_traits.hpp"
 namespace ft{
-    template <class Iterator>
-    struct iterator_traits{
-        typedef typename Iterator::value_type                   value_type;
-        typedef typename Iterator::difference_type              difference_type;
-        typedef typename Iterator::iterator_category            iterator_category;
-        typedef typename Iterator::pointer                      pointer;
-        typedef typename Iterator::reference                    reference;
-    };
-    template <class T*>
-    struct iterator_traits{
-        typedef  T                          value_type;
-        typedef size_t                      difference_type;
-        typedef random_access_iterator_tag  iterator_category;
-        typedef T*                          pointer;
-        typedef T&                          reference;
-    };
-    struct iterator_traits
+    
     template <class T>
     class iterator{
         typedef typename iterator_traits<T *>::value_type               value_type;
@@ -26,96 +23,49 @@ namespace ft{
         typedef typename iterator_traits<T *>::iterator_category        iterator_category;
         typedef typename iterator_traits<T *>::pointer                  pointer;
         typedef typename iterator_traits<T *>::reference                reference; 
-        private:
-            pointer ptr;
         public:
-            iterator() {
-                ptr = NULL;
-            }
-            iterator(T *p){
-                ptr = p;
-            }
-            iterator(iterator& p){
-                *this = p;
-            }
+            iterator() {ptr = NULL;}
+            iterator(pointer p){ptr = p;}
+            iterator(iterator& p){*this = p;}
             T& operator=(const iterator& to_copy){
                 ptr = to_copy.ptr;
-                return (*this);
-            }
-            bool operator==(iterator &a)
-            {
-                return ptr == a.ptr;
-            }
-            bool operator!=(iterator a)
-            {
-                return ptr != a.ptr;
-            }
-            bool operator<(iterator &a)
-            {
-                return ptr < a.ptr;
-            }
-            bool operator>(iterator &a)
-            {
-                return ptr > a.ptr;
-            }
-            bool operator<=(iterator &a)
-            {
-                return ptr <= a.ptr;
-            }
-            bool operator>=(iterator &a)
-            {
-                return ptr >= a.ptr;
-            }
-            T* operator->()
-            {
-                return (ptr);
-            }
-            T& operator*()
-            {
-                return (*ptr);
-            }
-            iterator& operator+(int i)
-            {
+                return (*this);}
+            bool operator==(iterator &a){return ptr == a.ptr;}
+            bool operator!=(iterator a){return ptr != a.ptr;}
+            bool operator<(iterator &a){return ptr < a.ptr;}
+            bool operator>(iterator &a){return ptr > a.ptr;}
+            bool operator<=(iterator &a){return ptr <= a.ptr;}
+            bool operator>=(iterator &a){return ptr >= a.ptr;}
+            pointer operator->(){return (ptr);}
+            reference operator*(){return (*ptr);}
+            iterator& operator+(int i){
                 ptr += i;
-                return (*this);
-            }
-            iterator& operator-(int i)
-            {
+                return (*this);}
+            iterator& operator-(int i){
                 ptr -= i;
-                return (*this);
-            }
-            iterator& operator+(iterator& i)
-            {
+                return (*this);}
+            iterator& operator+(iterator& i){
                 ptr += i.ptr;
-                return (*this);
-            }
-            iterator& operator-(iterator& i)
-            {
+                return (*this);}
+            iterator& operator-(iterator& i){
                 ptr -= i.ptr;
-                return (*this);
-            }
-            iterator& operator++()
-            {
+                return (*this);}
+            iterator& operator++(){
                 ptr++;
-                return (*this);
-            }
-            iterator operator++(int)
-            {
+                return (*this);}
+            iterator operator++(int){
                 iterator temp = *this;
                 ++ptr;
-                return (temp);
-            }
-            iterator& operator--()
-            {
+                return (temp);}
+            iterator& operator--(){
                 ptr--;
-                return (*this);
-            }
-            iterator operator--(int)
-            {
+                return (*this);}
+            iterator operator--(int){
                 iterator temp = *this;
                 --ptr;
-                return (temp);
-            }
+                return (temp);}
+        private:
+            pointer ptr;
     };
 }
 
---
+++ util/iterator_traits.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iterator_traits.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilefhail <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/30 15:13:20 by ilefhail          #+#    #+#             */
+/*   Updated: 2023/01/30 15:13:25 by ilefhail         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TRAITS_HPP
+#define TRAITS_HPP
+
+namespace ft{
+    template <class Iterator>
+    struct iterator_traits{
+        typedef typename Iterator::value_type                   value_type;
+        typedef typename Iterator::difference_type              difference_type;
+        typedef typename Iterator::iterator_category            iterator_category;
+        typedef typename Iterator::pointer                      pointer;
+        typedef typename Iterator::reference                    reference;
+    };
+    template <class T>
+    struct iterator_traits<T*>{
+        typedef  T                          value_type;
+        typedef size_t                      difference_type;
+        typedef std::random_access_iterator_tag  iterator_category;
+        typedef T*                          pointer;
+        typedef T&                          reference;
+    };
+}
+
+
+
+
+
+#endif
